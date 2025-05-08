9d8bf00721c7ab1dac5ed732b43c71f0b6af11cc
1ch <11487838+L0sted@users.noreply.github.com>
2025-05-08 09:58:18 +0300

    Update pmbootstram-clean.sh

--- pmbootstram-clean.sh
+++ pmbootstram-clean.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-source ~/.profile
 pmbootstrap zap -p
+pmbootstrap zap -hc -d -p
 pmbootstrap shutdown
 export WORK=$(pmbootstrap config work)
 sudo rm -rf "$WORK"
 rm -f ~/.config/pmbootstrap.cfg
-rm -f ~/.local/bin/pmbootstrap
-rm -rf pmbootstrap
+rm -f ~/.config/pmbootstrap_v3.cfg
